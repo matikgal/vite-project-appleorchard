9ae86387df95bd3214aae571b74367bc64d09e4f
Mateusz <77492310+matikgal@users.noreply.github.com>
2024-10-04 15:23:10 +0200

    Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,17 @@
 export default function NavBar() {
-	return <div></div>
+	return (
+		<div>
+			<div>
+				<p>Open for the season!</p>
+				<p>Store & Bakery Open daily 9am-5:30pm</p>
+			</div>
+			<div>
+				<a href=""></a>
+				<a href=""></a>
+
+				<a href=""></a>
+				<a href=""></a>
+			</div>
+		</div>
+	)
 }
