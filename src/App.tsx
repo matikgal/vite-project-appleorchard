6bb1a5173bbdd7a68bdecd0033b5fd195815a3bb
Mateusz <77492310+matikgal@users.noreply.github.com>
2024-10-05 20:35:59 +0200

    HeroImg

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import NavBar from './components/NavBar'
 import HeroImage from './components/HeroImage'
+import HomePageSection1 from './components/HomePageSection1'
 
 function App() {
 	return (
 		<>
 			<NavBar />
 			<HeroImage />
+			<HomePageSection1 />
 		</>
 	)
 }
