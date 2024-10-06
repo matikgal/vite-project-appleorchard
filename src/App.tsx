af0c7cd2b1f9d9f4259cf702c2fbb2eea23d88b3
Mateusz <77492310+matikgal@users.noreply.github.com>
2024-10-06 19:15:25 +0200

    section2

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import HomePage from './pages/HomePage'
 import OurOrchard from './pages/OurOrchard'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
@@ -11,6 +12,22 @@ function App() {
 					<Route path="/OurOrchard" element={<OurOrchard />} />
 				</Routes>
 			</BrowserRouter>
+=======
+import NavBar from './components/NavBar'
+import HeroImage from './components/HeroImage'
+import HomePageSection1 from './components/HomePageSection1'
+import OurOrchardHeroImg from './components/ourOrchardHeroImg'
+import HomePageSection2 from './components/HomePageSection2'
+
+function App() {
+	return (
+		<>
+			<NavBar />
+			<HeroImage />
+			<HomePageSection1 />
+			<HomePageSection2 />
+			<OurOrchardHeroImg />
+>>>>>>> Stashed changes
 		</>
 	)
 }
---
+++ src/components/HomePageSection2.tsx
@@ -0,0 +1,14 @@
+export default function HomePageSection2() {
+	return (
+		<div className="w-full xl:text-lg font-medium text-sm">
+			<div className="bg-[url('images/cider-house-image.jpg')] w-full  bg-no-repeat bg-cover bg-center flex flex-col-reverse h-[30vh] lg:h-[50vh] "></div>
+			<div>
+				<div className="bg-[#16472D] flex flex-col justify-center items-center py-7 md:py-10 lg:py-12">
+					<p className="text-[#FDF1DE] text-lg md:text-2xl lg:text-3xl">plan your jonamac orchard visit</p>
+					<h3 className="text-[#CF9A29] text-5xl md:text-6xl lg:text-7xl">Pick, Sip & Play</h3>
+				</div>
+			</div>
+            
+		</div>
+	)
+}
