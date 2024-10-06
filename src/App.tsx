323fa25eaf864e071399cd7cc25b35dfbe21756b
Mateusz <77492310+matikgal@users.noreply.github.com>
2024-10-06 19:17:35 +0200

    section2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import HomePage from './pages/HomePage'
 import OurOrchard from './pages/OurOrchard'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
@@ -12,22 +11,6 @@ function App() {
 					<Route path="/OurOrchard" element={<OurOrchard />} />
 				</Routes>
 			</BrowserRouter>
-=======
-import NavBar from './components/NavBar'
-import HeroImage from './components/HeroImage'
-import HomePageSection1 from './components/HomePageSection1'
-import OurOrchardHeroImg from './components/ourOrchardHeroImg'
-import HomePageSection2 from './components/HomePageSection2'
-
-function App() {
-	return (
-		<>
-			<NavBar />
-			<HeroImage />
-			<HomePageSection1 />
-			<HomePageSection2 />
-			<OurOrchardHeroImg />
->>>>>>> Stashed changes
 		</>
 	)
 }
